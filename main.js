64fd83e0fbb1525549bdd10fe92efe2ad4cf3e06
Husaini <husainimuhd99@yahoo.com>
2022-07-03 23:03:20 +0800

    Added Random and Message Generator function

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ let astrology = ['Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer',
 let sentiment = ['bad', 'very bad', 'good', 'very good'];
 
 let status = ['chance to win in lottery.', 'fortune incoming.', 'day ahead.', 'future.', 'way of life ahead.'];
+
+function randomSelection(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+function astrologyMessage(astrology, sentiment, status) {
+    let astrologyPart = randomSelection(astrology);
+    let sentimentPart = randomSelection(sentiment);
+    let statusPart = randomSelection(status);
+
+    let message = `${astrologyPart} will have a ${sentimentPart} ${statusPart}`;
+    return message;
+}
