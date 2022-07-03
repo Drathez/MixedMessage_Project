d5a9a1f658accd9a493dd6139b58502e1ad872ee
Husaini <husainimuhd99@yahoo.com>
2022-07-03 22:53:29 +0800

    Data Addition

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // Mixed Message is a message generator program where 
 // every time a user runs a program, they should get a new, randomized messages as output.
 
+// This project is an astrology message generator
+
+let astrology = ['Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer',
+                 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+
+let sentiment = ['bad', 'very bad', 'good', 'very good'];
+
+let status = ['chance to win in lottery.', 'fortune incoming.', 'day ahead.', 'future.', 'way of life ahead.'];
